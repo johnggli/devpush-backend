38b23653a94d550cb26dd840c13c99e1e71d8d1c
John Emerson <john.carv.sousa@gmail.com>
2023-09-26 22:33:43 -0300

    Refactor: remove plugin-proposal-private-property-in-object

--- babel.config.js
+++ babel.config.js
@@ -54,12 +54,6 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-proposal-private-property-in-object',
-        {
-          loose: true
-        }
-      ],
-      [
         '@babel/plugin-transform-runtime',
         {
           helpers: false
