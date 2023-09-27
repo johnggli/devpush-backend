0155b136cc5377f0e8d80dafc4984dfdb66f398b
John Emerson <john.carv.sousa@gmail.com>
2023-09-26 22:27:26 -0300

    Refactor: remove plugin-proposal-private-methods

--- babel.config.js
+++ babel.config.js
@@ -54,12 +54,6 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-proposal-private-methods',
-        {
-          loose: true
-        }
-      ],
-      [
         '@babel/plugin-proposal-private-property-in-object',
         {
           loose: true
